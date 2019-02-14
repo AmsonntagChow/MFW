feacf419f73170e8a954154394a2413ac017f216
mio <a375164079@gmail.com>
2019-02-14 09:11:54 +0800

    233

--- site/1.js
+++ site/1.js
@@ -1,5 +1,13 @@
 var username;
 var message;
+var hotel = {
+	name: 'Quay',
+	rooms: 40,
+	booked: 25,
+	checkAvailability:function() {
+		return this.rooms - this.booked;
+	}
+};
 
 username = 'Amos';
 message = 'See our upcoming range';
@@ -8,3 +16,6 @@ var elName = document.getElementById('name');
 elName.textContent = username;
 var elNote = document.getElementById('note');
 elNote.textContent = message;
+var elRooms = document.getElementById('rooms')
+elRooms.textContent = hotel.
+checkAvailability();
